d9e7c732645f05ccef97315ff081b1f49c956fb6
Suraj <mayur.1496@gmail.com>
2020-08-18 15:16:35 +0530

    18/8

---
+++ coinCombinations1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <bitset>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define sz(x) (x).size()
+auto speedup=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+#define M 1000000007
+
+int main(){
+	ll n, sum;
+	cin >> n >> sum;
+	vector<int> c(n);
+	vector<ll> m(1000001, 0);
+	m[0] = 0;
+	for(int i=0; i<n; i++) cin >> c[i], m[c[i]] = 1;
+	for(int i=1; i<=sum; i++){
+		for(int x: c){
+			if(i-x >= 0) m[i] = (m[i] + m[i-x])%M;
+		}
+	}
+	cout << m[sum] << endl;
+}
